1e89ccef5c3545a0935f6235639385498298740c
andreasbhansen <andreasb.nor@gmail.com>
2015-03-23 17:22:13 +0100

    Added user registration

--- client/helpers/login.js
+++ client/helpers/login.js
@@ -1,6 +1,25 @@
 Template.Login.events({
 
-    'click #login' : function(e, t){
+    'click #register': function (e)
+    {
+        e.preventDefault();
+        var email = document.getElementById('register-email').value;
+        var password = document.getElementById('register-password').value;
+
+        Accounts.createUser({email: email, password: password}, function (err) {
+            if (err) {
+                console.log(err);
+                alert(err.reason);
+            } else {
+                Router.go('/profile');
+            }
+
+        });
+
+    },
+
+    'click #login': function (e, t)
+    {
         e.preventDefault();
         // retrieve the input field values
         var email = document.getElementById('login-email').value;
@@ -10,22 +29,26 @@ Template.Login.events({
 
         // If validation passes, supply the appropriate fields to the
         // Meteor.loginWithPassword() function.
-        Meteor.loginWithPassword(email, password, function(err){
+        Meteor.loginWithPassword(email, password, function (err)
+        {
 
-            if (err) {
+            if (err)
+            {
                 alert(err.reason);
                 console.log(err);
-            } else {
+            }
+            else
+            {
                 console.log('Logged in');
-                Router.go('/')
+                Router.go('/profile')
             }
 
             /*if (err);
-            // The user might not have been found, or their passwword
-            // could be incorrect. Inform the user that their
-            // login attempt has failed.
-            else
-            // The user has been logged in.*/
+             // The user might not have been found, or their passwword
+             // could be incorrect. Inform the user that their
+             // login attempt has failed.
+             else
+             // The user has been logged in.*/
         });
         return false;
     }
